5257a3158d4b2c1b6972dfabfe2ee6fda28fb454
4oko4ow <cerberussoftware823@gmail.com>
2026-02-27 20:17:34 +0700

    feat: scaffold unified solanaidle program

---
+++ programs/solanaidle/src/state/mod.rs
@@ -0,0 +1,7 @@
+pub mod boss_state;
+pub mod player_progress;
+pub mod vrf_result;
+
+pub use boss_state::BossState;
+pub use player_progress::PlayerProgress;
+pub use vrf_result::{VrfResult, STATUS_FULFILLED, STATUS_PENDING};
